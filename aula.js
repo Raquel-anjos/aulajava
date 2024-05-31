095667cb853ee26f16c2a102351fe2a00cdaa0e3
Raquel Anjos <rmouzinhoanjos@gmail.com>
2024-05-31 16:17:43 -0300

    aula js exercicio de estrutura condicional

---
+++ aula.js
@@ -0,0 +1,16 @@
+
+
+const numero = 70
+ 
+if (numero > 10) {
+    console.log("Esse número é maior que 10")
+} else if (numero = 10) {
+    console.log("Esse número é 10")
+
+}
+    else if (numero < 10){
+        console.log("Esse número é menor que 10")
+    }
+
+    
+
